5ef1862f555938e737097b06b4b97e59b916479d
NemesLaszlo <wow.laszlo@gmail.com>
2021-01-26 21:10:44 +0100

    MongoDB setup and connect some mirror fix

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './database/database.js'
+import products from './data/products.js'
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.send('API is running!')
@@ -19,6 +26,6 @@ app.get('/api/products/:id', (req, res) => {
 
 app.listen(port, () =>
   console.log(
-    `Server listening at http://localhost:${port}`
+    `Server listening at http://localhost:${port} and running mode is ${process.env.NODE_ENV}`.yellow.bold
   )
 )
