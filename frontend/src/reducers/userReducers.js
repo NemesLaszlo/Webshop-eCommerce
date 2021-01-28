687a7913189b513e1b50fe94545c516d129654a0
NemesLaszlo <wow.laszlo@gmail.com>
2021-01-28 22:58:22 +0100

    ProfileScreen getUserDetails and updateUserProfile

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,5 +1,7 @@
 import {USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT, 
-        USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL} from '../constants/userConstants'
+        USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL,
+        USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_DETAILS_FAIL,
+        USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_RESET} from '../constants/userConstants'
 
 export const userLoginReducer = (state={}, action) => {
     switch(action.type) {
@@ -23,6 +25,32 @@ export const userRegisterReducer = (state={}, action) => {
         case USER_REGISTER_SUCCESS:
             return { loading: false, userInfo: action.payload }
         case USER_REGISTER_FAIL:
+            return { loading: false, error: action.payload }
+        default:
+            return state
+    }
+}
+
+export const userDetailsReducer = (state={user : {}}, action) => {
+    switch(action.type) {
+        case USER_DETAILS_REQUEST:
+            return {...state, loading: true }
+        case USER_DETAILS_SUCCESS:
+            return { loading: false, user: action.payload }
+        case USER_DETAILS_FAIL:
+            return { loading: false, error: action.payload }
+        default:
+            return state
+    }
+}
+
+export const userUpdateProfileReducer = (state={}, action) => {
+    switch(action.type) {
+        case USER_UPDATE_PROFILE_REQUEST:
+            return { loading: true }
+        case USER_UPDATE_PROFILE_SUCCESS:
+            return { loading: false, success: true, userInfo: action.payload }
+        case USER_UPDATE_PROFILE_FAIL:
             return { loading: false, error: action.payload }
         default:
             return state
