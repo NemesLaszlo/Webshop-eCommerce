c11f9aed5ec013902c4308a43e5d7e1c302db663
NemesLaszlo <wow.laszlo@gmail.com>
2021-01-28 18:07:09 +0100

    authUser resigterUser authMiddleware and get userProfile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express'
 const router = express.Router()
 
-import {authUser} from '../controllers/userController.js'
+import {protect} from '../middleware/authMiddleware.js'
+import {authUser, registerUser, getUserProfile} from '../controllers/userController.js'
 
+router.route('/').post(registerUser)
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
