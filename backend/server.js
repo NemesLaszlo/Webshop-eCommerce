af8ec1a32136530eb9e1ce12be42e12d5ce5f95c
NemesLaszlo <wow.laszlo@gmail.com>
2021-01-28 17:04:47 +0100

    product and user routes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import connectDB from './database/database.js'
+
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 connectDB()
@@ -11,11 +13,14 @@ connectDB()
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API is running!')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
