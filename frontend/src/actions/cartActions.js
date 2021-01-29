1c02e9e24df4dbda94c9edef48c04fa19722ec5b
NemesLaszlo <wow.laszlo@gmail.com>
2021-01-29 16:40:41 +0100

    paymentMethod to store and PlaceOrderScreen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -16,7 +16,6 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
     })
     
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-
 }
 
 export const removeFromCart = (id) => (dispatch, getState) => {
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,6 +1,6 @@
 import {CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS, CART_SAVE_PAYMENT_METHOD} from '../constants/cartConstants'
 
-export const cartReducer = (state={cartItems: [], shippingAddress: {}}, action) => {
+export const cartReducer = (state={cartItems: [], shippingAddress: {}, paymentMethod: {}}, action) => {
     switch(action.type) {
         case CART_ADD_ITEM:
             const item = action.payload
