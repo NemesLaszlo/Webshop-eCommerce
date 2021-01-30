0aef06b86e3233a453d2e3e6ef8cedb36c527b24
NemesLaszlo <wow.laszlo@gmail.com>
2021-01-30 12:07:22 +0100

    OrderScreen and Backend Endpoints to getOrderDetails and updateOrderPay

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,5 +1,6 @@
 import {ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL,
-        ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL} from '../constants/orderConstants'
+        ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL,
+        ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_RESET} from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
     switch(action.type) {
@@ -14,7 +15,7 @@ export const orderCreateReducer = (state = {}, action) => {
     }
 }
 
-export const orderDetailsReducer = (state = {orderItems: [], shippingAddress: {}}, action) => {
+export const orderDetailsReducer = (state = {loading: true, orderItems: [], shippingAddress: {}}, action) => {
     switch(action.type) {
         case ORDER_DETAILS_REQUEST:
             return {...state, loading: true }
@@ -22,6 +23,21 @@ export const orderDetailsReducer = (state = {orderItems: [], shippingAddress: {}
             return {loading: false, order: action.payload}
         case ORDER_DETAILS_FAIL:
             return {loading: false, error: action.payload}
+        default:
+            return state
+    }
+}
+
+export const orderPayReducer = (state = {}, action) => {
+    switch(action.type) {
+        case ORDER_PAY_REQUEST:
+            return {loading: true }
+        case ORDER_PAY_SUCCESS:
+            return {loading: false, success: true}
+        case ORDER_PAY_FAIL:
+            return {loading: false, error: action.payload}
+        case ORDER_PAY_RESET:
+            return {}
         default:
             return state
     }
