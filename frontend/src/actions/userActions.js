8e8e21dda39710b0cac3bbae72b07a8dce430fbb
NemesLaszlo <wow.laszlo@gmail.com>
2021-01-31 16:42:59 +0100

    User Profile Orders Table and Logout Resets - user details and the user's orders

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 import {USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT,
         USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL,
-        USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_DETAILS_FAIL,
+        USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_DETAILS_FAIL, USER_DETAILS_RESET,
         USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_RESET} from '../constants/userConstants'
+import {ORDER_LIST_MY_RESET} from '../constants/orderConstants'
 
 export const login = (email, password) => async (dispatch) => {
     try {
@@ -30,9 +31,9 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
 
-    dispatch({
-        type: USER_LOGOUT
-    })
+    dispatch({type: USER_LOGOUT})
+    dispatch({type: USER_DETAILS_RESET})
+    dispatch({type: ORDER_LIST_MY_RESET})
 }
 
 export const register = (name, email, password) => async (dispatch) => {
