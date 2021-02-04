d7a72222b93978219401851e76932b04a8c6cb4d
NemesLaszlo <wow.laszlo@gmail.com>
2021-02-04 17:48:34 +0100

    Backend Admin getUserById and updateUserById

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,11 +2,11 @@ import express from 'express'
 const router = express.Router()
 
 import {protect, admin} from '../middleware/authMiddleware.js'
-import {authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser} from '../controllers/userController.js'
+import {authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserById, updateUser} from '../controllers/userController.js'
 
 router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
-router.route('/:id').delete(protect, admin, deleteUser)
+router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser)
 
 export default router
