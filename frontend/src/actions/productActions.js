6e0d714e9a62075793ad9bae684bb794db091852
NemesLaszlo <wow.laszlo@gmail.com>
2021-02-07 21:56:06 +0100

    Search option backend mod and frontend component with integration

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -6,10 +6,10 @@ import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL,
         PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_SUCCESS, PRODUCT_UPDATE_FAIL,
         PRODUCT_CREATE_REVIEW_REQUEST, PRODUCT_CREATE_REVIEW_SUCCESS, PRODUCT_CREATE_REVIEW_FAIL} from '../constants/productConstants'
 
-export const listProducts = () => async (dispatch) => {
+export const listProducts = (keyword = '') => async (dispatch) => {
     try {
         dispatch({type: PRODUCT_LIST_REQUEST})
-        const {data} = await axios.get('/api/products')
+        const {data} = await axios.get(`/api/products?keyword=${keyword}`)
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
             payload: data
