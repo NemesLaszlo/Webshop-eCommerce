31c1ad0a9831a0d1d2e47f47b42ed8d41c7f08ca
NemesLaszlo <wow.laszlo@gmail.com>
2021-02-08 16:09:41 +0100

    User Profile name change update in the header as well and Meta react-helmet

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -5,6 +5,7 @@ import {Row, Col, Image, ListGroup, Card, Button, Form} from 'react-bootstrap'
 import Rating from '../components/Rating'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
+import Meta from '../components/Meta'
 import {listProductDetails, createProductReview} from '../actions/productActions'
 import {PRODUCT_CREATE_REVIEW_RESET} from '../constants/productConstants'
 
@@ -50,7 +51,8 @@ const ProductScreen = ({history, match}) => {
         <>
             <Link className='btn btn-light my-3' to='/'>Go Back</Link>
             {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
-                <>
+            <>
+                <Meta title={product.name} />
                 <Row>
                     <Col md={6}>
                         <Image src={product.image} alt={product.name} fluid />
